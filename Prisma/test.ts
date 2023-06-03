dc65a88e988a9afea5284843e21c3fc7b40dd66a
yassine-miraoui <yassinemiraoui4@gmail.com>
2023-06-03 15:04:26 +0100

    good luck1

--- Prisma/test.ts
+++ Prisma/test.ts
@@ -3,7 +3,13 @@ const prisma = new PrismaClient()
 
 
 async function main() {
-const comments = await prisma.comments.findMany()
+const comments = await prisma.comments.create({
+    data: {
+      
+        bodyCom: 'sabrin',
+        id: 25
+    },
+  })
 console.log(comments)    
 }
 
@@ -13,3 +19,19 @@ main()
 .finally (async () => {
     await prisma.$disconnect()
 }) 
+
+
+
+
+// const users = prisma.profissionalUser;
+
+// const getAll = async (req,res)=>{
+//     try{
+//     const profissionals = await users.findMany();
+//     res.status(200).json(profissionals)
+// }
+// catch(err){
+//     console.log(err)
+//      res.status(500).json(err)
+// }
+// }
