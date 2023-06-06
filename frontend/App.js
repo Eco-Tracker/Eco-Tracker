ace468664a97e5bb37acd11727b2a4462d46424f
yassine-miraoui <yassinemiraoui4@gmail.com>
2023-06-06 19:16:09 +0100

    done

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,20 @@
-import { createStackNavigator, DefaultTheme } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Tabs from "../frontend/NavBar/Nav";
-import React from "react"
+import { createStackNavigator, DefaultTheme } from '@react-navigation/stack';
+
+
+
+import Home from './component/Home'
+import Home2 from './component/Home2'
+import Loginpers from './component/Loginpers.js';
+import Login from './component/Loginprof.js';
+import Signuppers from './component/Signuppers.js';
+import SignUpPro from './component/Signupprof.js';
+
+
+
+
+
+const Stack=createStackNavigator()
 
 
 const App=() => { 
@@ -10,8 +23,18 @@ const App=() => {
 
   return (
 
-     <NavigationContainer>
-      <Tabs />
+     <NavigationContainer  >
+      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName='Home'>
+       <Stack.Screen   name='Home'  component={Home}/>
+       <Stack.Screen   name='Home2'  component={Home2}/>
+       <Stack.Screen   name='Loginpers'  component={Loginpers}/>
+       <Stack.Screen   name='Loginprof'  component={Login}/>
+       <Stack.Screen   name='Signuppers'  component={Signuppers}/>
+       <Stack.Screen   name='SignUpPro'  component={SignUpPro}/>
+       
+      
+
+      </Stack.Navigator>
     </NavigationContainer>
 
 
@@ -21,9 +44,6 @@ const App=() => {
 
 
 export default App;
-
-
-
 
 
 
